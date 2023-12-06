9fd0ca2e13dcd62ff01dd88dc6e4651ba21cb658
rosserl <rosserl@mail.gvsu.edu>
2023-12-06 12:12:26 -0500

    added score to You Win screen not reactive though

--- src/cis350doom/object_renderer.py
+++ src/cis350doom/object_renderer.py
@@ -36,6 +36,11 @@ class ObjectRenderer:
     def win_game(self):
         self.screen.blit(self.game_win_image,(0,0))
 
+        font = pg.font.Font(None, 36)
+        text = font.render("Score: 5", True, (255, 255, 255))
+        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 100))
+        self.screen.blit(text, text_rect)
+
     def draw_player_health(self):
         health = str(self.game.player.health)
         for i, char in enumerate(health):
